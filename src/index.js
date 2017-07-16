17ff2e49bc58fb64aacc3855dc18a059bde6f6c0
Strandol <dvalian@mail.ru>
2017-07-16 20:08:07 +0300

    create mail list

---
+++ src/index.js
@@ -0,0 +1,29 @@
+let app = angular.module('userMail', []);
+
+app.controller('userController', function($scope) {
+    $.get('https://api.myjson.com/bins/kpg5f', (response) => {
+        $scope.messages = response;
+        $scope.$applyAsync();
+    }).fail((err) => {
+        alert(err);
+    })
+})
+
+app.directive('message', () => {
+    return {
+        restrict: 'E',
+        template: `
+            <li ng-model="opened">
+                <h2>{{msg.addresser}}</h2>
+                <h3>{{msg.title}}</h3>
+                <p>{{msg.date}}</p>
+                <button ng-click="opened=!opened">Open message</button>
+                <div ng-show="opened">
+                    <h4>{{msg.addresser}}</h4>
+                    <img src={{msg.addresserImg}}>
+                    <p>{{msg.message}}</p>
+                </div>
+            </li>
+        `
+    }
+})
