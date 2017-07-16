26fdb59066a9b15ba14998715b10fb8679cf68be
Strandol <dvalian@mail.ru>
2017-07-16 20:15:53 +0300

    ng-model

--- src/index.js
+++ src/index.js
@@ -13,19 +13,11 @@ app.directive('message', () => {
     return {
         restrict: 'E',
         template: `
-<<<<<<< HEAD
             <li>
                 <h2>{{msg.addresser}}</h2>
                 <h3>{{msg.title}}</h3>
                 <p>{{msg.date}}</p>
                 <button ng-model="opened" ng-click="opened=!opened">Open message</button>
-=======
-            <li ng-model="opened">
-                <h2>{{msg.addresser}}</h2>
-                <h3>{{msg.title}}</h3>
-                <p>{{msg.date}}</p>
-                <button ng-click="opened=!opened">Open message</button>
->>>>>>> 17ff2e49bc58fb64aacc3855dc18a059bde6f6c0
                 <div ng-show="opened">
                     <h4>{{msg.addresser}}</h4>
                     <img src={{msg.addresserImg}}>
