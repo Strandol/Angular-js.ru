39d4f729a3ad9ce9e73b506b572ef8864a818beb
Strandol <dvalian@mail.ru>
2017-07-21 15:40:50 +0300

    decomposition

--- src/index.js
+++ src/index.js
@@ -74,49 +74,6 @@ app.controller('UserController', function(MsgService, ContactService) {
     }
 })
 
-app.config(($stateProvider) => {
-    $stateProvider
-        .state('index', {
-            url: '/',
-            template: `
-                <message ng-repeat="msg in userController.selectedUser.inputMsgs"></message>
-            `
-        })
-        .state('contacts', {
-            url: ':email/contacts',
-            template: `
-                <div class="contacts">
-                    <contacts ng-repeat="contact in userController.selectedUser.contacts"></contacts>
-                    <h2 class="noContactsHeader" ng-show="!userController.selectedUser.contacts.length">Контактов не записано...</h2>
-                </div>
-            `
-        })
-        .state('messages', {
-            url: ':email/messages',
-            template: `
-                <message ng-repeat="msg in userController.selectedMsgs"></message>
-            `
-        })
-        .state('messages.input', {
-            url: '/input',
-            template: `
-                <message ng-repeat="msg in userController.selectedUser.inputMsgs"></message>
-            `
-        })
-        .state('messages.output', {
-            url: '/output',
-            template: `
-                <message ng-repeat="msg in userController.selectedUser.outputMsgs"></message>
-            `
-        })
-        .state('messages.marked', {
-            url: '/marked',
-            template: `
-                <message ng-repeat="msg in userController.selectedUser.markedMsgs"></message>
-            `
-        })
-})
-
 app.service('MsgService', function($q) {
     this.loadUsers = function() {
         return $q((resolve, reject) => {
